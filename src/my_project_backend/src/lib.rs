ce17494e345e50327ffe5f44c1f50844621a0559
pu0238 <pu0238@protonmail.com>
2024-12-03 17:12:21 +0000

    added validation

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -10,9 +10,13 @@ thread_local! {
 // komentarze ?
 
 #[ic_cdk::update]
-fn add_blog(title: String, content: String, tags: Vec<String>) {
+fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<String, String>{
+    if title.len() > 250 {
+        return Err("Title is too long!".to_string())
+    }
     let blog = Blog::new(title, content, tags);
     BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
+    Ok("Added new blog".to_string())
 }
 
 #[ic_cdk::query]
