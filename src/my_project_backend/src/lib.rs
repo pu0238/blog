caaab25868d744e29c5acda31f7348cdcbd8cdc8
pu0238 <pu0238@protonmail.com>
2024-12-03 17:29:00 +0000

    Added Retunn of latest blog

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -10,13 +10,20 @@ thread_local! {
 // komentarze ?
 
 #[ic_cdk::update]
-fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<String, String>{
+fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, String>{
     if title.len() > 250 {
         return Err("Title is too long!".to_string())
     }
     let blog = Blog::new(title, content, tags);
     BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
-    Ok("Added new blog".to_string())
+    let last_blog = BLOGS.with(|blogs| 
+        blogs
+        .borrow()
+        .last()
+        .expect("Vec should not be empty")
+        .clone()
+    );
+    Ok(last_blog)
 }
 
 #[ic_cdk::query]
