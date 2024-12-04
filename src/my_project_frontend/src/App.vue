df9b934c8bd7f0613b0ef6cb1d60694a358d91c7
pu0238 <pu0238@protonmail.com>
2024-12-04 16:07:15 +0000

    Added blogs on frontend

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
-let greeting = ref('');
+let blogs = ref([]);
 
 async function handleSubmit(e) {
   e.preventDefault();
@@ -12,7 +12,19 @@ async function handleSubmit(e) {
   const splitedTags = tags.split(",")
 
   await my_project_backend.add_blog(title, content, splitedTags)
+  await getBlogs()
 }
+
+async function getBlogs() {
+  const tempBlogs = await my_project_backend.get_blogs()
+  blogs.value = tempBlogs.map((blog) => {
+    return {
+      ...blog,
+      date: blog.date.toString()
+    }
+  })
+}
+getBlogs()
 </script>
 
 <template>
@@ -26,5 +38,6 @@ async function handleSubmit(e) {
       <div><p>Tags: </p><input id="tags" alt="tags" type="text" /></div>
       <button type="submit">Click to add!</button>
     </form>
+    {{ blogs }}
   </main>
 </template>
